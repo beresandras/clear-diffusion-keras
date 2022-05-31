641f75f981dd1b82683ca4adb7b99c6a51db8347
beresandras <30826103+beresandras@users.noreply.github.com>
2022-05-31 20:13:53 +0200

    metrics, dataset added

---
+++ dataset.py
@@ -0,0 +1,141 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+
+def round_to_int(float_value):
+    return tf.cast(tf.math.round(float_value), dtype=tf.int32)
+
+
+def preprocess_birds(image_size, padding=0.25):
+    def preprocess_image(data):
+        # unnormalize bounding box coordinates
+        height = tf.cast(tf.shape(data["image"])[0], dtype=tf.float32)
+        width = tf.cast(tf.shape(data["image"])[1], dtype=tf.float32)
+        bounding_box = data["bbox"] * tf.stack([height, width, height, width])
+
+        # calculate center, length of longer side, add padding
+        target_center_y = 0.5 * (bounding_box[0] + bounding_box[2])
+        target_center_x = 0.5 * (bounding_box[1] + bounding_box[3])
+        target_size = tf.maximum(
+            (1.0 + padding) * (bounding_box[2] - bounding_box[0]),
+            (1.0 + padding) * (bounding_box[3] - bounding_box[1]),
+        )
+
+        # modify bounding box to fit into image
+        target_height = tf.reduce_min(
+            [target_size, 2.0 * target_center_y, 2.0 * (height - target_center_y)]
+        )
+        target_width = tf.reduce_min(
+            [target_size, 2.0 * target_center_x, 2.0 * (width - target_center_x)]
+        )
+
+        # crop image
+        image = tf.image.crop_to_bounding_box(
+            data["image"],
+            offset_height=round_to_int(target_center_y - 0.5 * target_height),
+            offset_width=round_to_int(target_center_x - 0.5 * target_width),
+            target_height=round_to_int(target_height),
+            target_width=round_to_int(target_width),
+        )
+
+        # resize and clip
+        image = tf.image.resize(
+            image, size=[image_size, image_size], method=tf.image.ResizeMethod.AREA
+        )
+        return tf.clip_by_value(image / 255.0, 0.0, 1.0)
+
+    return preprocess_image
+
+
+def preprocess_flowers(image_size):
+    def preprocess_image(data):
+        # center crop image
+        height = tf.shape(data["image"])[0]
+        width = tf.shape(data["image"])[1]
+        crop_size = tf.minimum(height, width)
+        image = tf.image.crop_to_bounding_box(
+            data["image"],
+            (height - crop_size) // 2,
+            (width - crop_size) // 2,
+            crop_size,
+            crop_size,
+        )
+
+        # resize and clip
+        image = tf.image.resize(
+            image, size=[image_size, image_size], method=tf.image.ResizeMethod.AREA
+        )
+        return tf.clip_by_value(image / 255.0, 0.0, 1.0)
+
+    return preprocess_image
+
+
+def preprocess_celeba(image_size, crop_size=140):
+    def preprocess_image(data):
+        # center crop image
+        height = 218
+        width = 178
+        image = tf.image.crop_to_bounding_box(
+            data["image"],
+            (height - crop_size) // 2,
+            (width - crop_size) // 2,
+            crop_size,
+            crop_size,
+        )
+
+        # resize and clip
+        image = tf.image.resize(
+            image, size=[image_size, image_size], method="bicubic", antialias=True
+        )
+        return tf.clip_by_value(image / 255.0, 0.0, 1.0)
+
+    return preprocess_image
+
+
+def preprocess_cifar(image_size):
+    def preprocess_image(data):
+        # will always have a resolution of 32x32
+        return tf.image.convert_image_dtype(data["image"], tf.float32)
+
+    return preprocess_image
+
+
+def prepare_dataset(dataset_name, split, image_size, batch_size):
+    preprocessors = {
+        "caltech_birds2011": preprocess_birds,
+        "oxford_flowers102": preprocess_flowers,
+        "celeb_a": preprocess_celeba,
+        "cifar10": preprocess_cifar,
+    }
+    preprocess_image = preprocessors[dataset_name](image_size)
+
+    split_index = {"train": 0, "validation": 1}
+    split_names = {
+        "caltech_birds2011": ["train", "test"],
+        "oxford_flowers102": [
+            "train[:70%]+validation[:70%]+test[:70%]",
+            "train[70%:]+validation[70%:]+test[70%:]",
+        ],
+        "celeb_a": ["train", "validation"],
+        "cifar10": ["train", "test"],
+    }
+    split = split_names[dataset_name][split_index[split]]
+
+    # the validation dataset is shuffled as well, because data order matters
+    # for the KID calculation
+    return (
+        tfds.load(
+            dataset_name,
+            data_dir="D:/Documents/datasets/tensorflow/",
+            split=split,
+            shuffle_files=True,
+        )
+        .map(
+            preprocess_image,
+            num_parallel_calls=tf.data.experimental.AUTOTUNE,
+        )
+        .cache()
+        .shuffle(10 * batch_size)
+        .batch(batch_size, drop_remainder=True)
+        .prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+    )
---
+++ metrics.py
@@ -0,0 +1,78 @@
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+class KID(keras.metrics.Metric):
+    def __init__(self, name="kid", input_shape=None, image_size=None, **kwargs):
+        super().__init__(name=name, **kwargs)
+
+        # resolution of images for the KID estimation
+        self.image_size = image_size
+
+        # KID is estimated per batch and is averaged across batches
+        self.kid_tracker = keras.metrics.Mean()
+
+        # a pretrained InceptionV3 is used without its classification layer
+        self.encoder = keras.Sequential(
+            [
+                layers.InputLayer(input_shape=input_shape),
+                layers.Lambda(self.preprocess_input),
+                keras.applications.InceptionV3(
+                    include_top=False,
+                    input_shape=(self.image_size, self.image_size, 3),
+                    weights="imagenet",
+                ),
+                layers.GlobalAveragePooling2D(),
+            ]
+        )
+
+    def preprocess_input(self, images):
+        images = tf.image.resize(
+            images,
+            size=[self.image_size, self.image_size],
+            method="bicubic",
+            antialias=True,
+        )
+        images = tf.clip_by_value(images, 0.0, 1.0)
+        images = keras.applications.inception_v3.preprocess_input(images * 255.0)
+        return images
+
+    def polynomial_kernel(self, features_1, features_2):
+        feature_dimensions = tf.cast(tf.shape(features_1)[1], dtype=tf.float32)
+        return (features_1 @ tf.transpose(features_2) / feature_dimensions + 1.0) ** 3.0
+
+    def update_state(self, real_images, generated_images, sample_weight=None):
+        real_features = self.encoder(real_images, training=False)
+        generated_features = self.encoder(generated_images, training=False)
+
+        # compute polynomial kernels using the two sets of features
+        kernel_real = self.polynomial_kernel(real_features, real_features)
+        kernel_generated = self.polynomial_kernel(
+            generated_features, generated_features
+        )
+        kernel_cross = self.polynomial_kernel(real_features, generated_features)
+
+        # estimate the squared maximum mean discrepancy using the average kernel values
+        batch_size = tf.shape(real_features)[0]
+        batch_size_f = tf.cast(batch_size, dtype=tf.float32)
+        mean_kernel_real = tf.reduce_sum(kernel_real * (1.0 - tf.eye(batch_size))) / (
+            batch_size_f * (batch_size_f - 1.0)
+        )
+        mean_kernel_generated = tf.reduce_sum(
+            kernel_generated * (1.0 - tf.eye(batch_size))
+        ) / (batch_size_f * (batch_size_f - 1.0))
+        mean_kernel_cross = tf.reduce_mean(kernel_cross)
+        kid = mean_kernel_real + mean_kernel_generated - 2.0 * mean_kernel_cross
+
+        # update the average KID estimate
+        self.kid_tracker.update_state(kid)
+
+        return kid  # this is not necessary but useful for debugging
+
+    def result(self):
+        return self.kid_tracker.result()
+
+    def reset_states(self):
+        self.kid_tracker.reset_states()
