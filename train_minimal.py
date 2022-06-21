79738ad5b14fb4280b33d3247038f205b9d01561
beresandras <30826103+beresandras@users.noreply.github.com>
2022-06-21 16:31:17 +0200

    minimal script added

---
+++ train_minimal.py
@@ -0,0 +1,438 @@
+import os
+import math
+import matplotlib
+from matplotlib import pyplot as plt
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"  # suppress info-level logs
+matplotlib.use("Agg")
+
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import tensorflow_addons as tfa
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+tf.get_logger().setLevel("WARN")  # suppress info-level logs
+
+
+# data
+dataset_repetitions = 10
+num_epochs = 50
+image_size = 64
+kid_image_size = 75
+kid_diffusion_steps = 5
+plot_diffusion_steps = 20
+
+# sampling
+min_signal_rate = 0.03
+
+# architecture
+embedding_dims = 32
+widths = [32, 64, 96, 128]
+block_depth = 2
+
+# optimization
+batch_size = 64
+ema = 0.999
+learning_rate = 1e-3
+weight_decay = 1e-4
+
+
+def preprocess_image(data):
+    # center crop image
+    height = tf.shape(data["image"])[0]
+    width = tf.shape(data["image"])[1]
+    crop_size = tf.minimum(height, width)
+    image = tf.image.crop_to_bounding_box(
+        data["image"],
+        (height - crop_size) // 2,
+        (width - crop_size) // 2,
+        crop_size,
+        crop_size,
+    )
+
+    # resize and clip
+    image = tf.image.resize(
+        image, size=[image_size, image_size], method="bicubic", antialias=True
+    )
+    return tf.clip_by_value(image / 255.0, 0.0, 1.0)
+
+
+def prepare_dataset(split):
+    # the validation dataset is shuffled as well, because data order matters
+    # for the KID calculation
+    return (
+        tfds.load("oxford_flowers102", split=split, shuffle_files=True)
+        .map(preprocess_image, num_parallel_calls=tf.data.AUTOTUNE)
+        .cache()
+        .repeat(dataset_repetitions)
+        .shuffle(10 * batch_size)
+        .batch(batch_size, drop_remainder=True)
+        .prefetch(buffer_size=tf.data.AUTOTUNE)
+    )
+
+
+# load dataset
+train_dataset = prepare_dataset("train[:80%]+validation[:80%]+test[:80%]")
+val_dataset = prepare_dataset("train[80%:]+validation[80%:]+test[80%:]")
+
+
+class KID(keras.metrics.Metric):
+    def __init__(self, name="kid", **kwargs):
+        super().__init__(name=name, **kwargs)
+
+        # KID is estimated per batch and is averaged across batches
+        self.kid_tracker = keras.metrics.Mean()
+
+        # a pretrained InceptionV3 is used without its classification layer
+        # transform the pixel values to the 0-255 range, then use the same
+        # preprocessing as during pretraining
+        self.encoder = keras.Sequential(
+            [
+                layers.InputLayer(input_shape=(image_size, image_size, 3)),
+                # preprocessing.Rescaling(255.0),
+                # preprocessing.Resizing(height=kid_image_size, width=kid_image_size),
+                layers.Lambda(self.resize_input),
+                layers.Lambda(keras.applications.inception_v3.preprocess_input),
+                keras.applications.InceptionV3(
+                    include_top=False,
+                    input_shape=(kid_image_size, kid_image_size, 3),
+                    weights="imagenet",
+                ),
+                layers.GlobalAveragePooling2D(),
+            ],
+            name="inception_encoder",
+        )
+
+    def resize_input(self, images):
+        images = tf.image.resize(
+            images,
+            size=[kid_image_size, kid_image_size],
+            method="bicubic",
+            antialias=True,
+        )
+        images = tf.clip_by_value(images, 0.0, 1.0)
+        return images * 255.0
+
+    def polynomial_kernel(self, features_1, features_2):
+        feature_dimensions = tf.cast(tf.shape(features_1)[1], dtype=tf.float32)
+        return (features_1 @ tf.transpose(features_2) / feature_dimensions + 1.0) ** 3.0
+
+    def update_state(self, real_images, generated_images, sample_weight=None):
+        real_features = self.encoder(real_images, training=False)
+        generated_features = self.encoder(generated_images, training=False)
+
+        # compute polynomial kernels using the two sets of features
+        kernel_real = self.polynomial_kernel(real_features, real_features)
+        kernel_generated = self.polynomial_kernel(
+            generated_features, generated_features
+        )
+        kernel_cross = self.polynomial_kernel(real_features, generated_features)
+
+        # estimate the squared maximum mean discrepancy using the average kernel values
+        batch_size = tf.shape(real_features)[0]
+        batch_size_f = tf.cast(batch_size, dtype=tf.float32)
+        mean_kernel_real = tf.reduce_sum(kernel_real * (1.0 - tf.eye(batch_size))) / (
+            batch_size_f * (batch_size_f - 1.0)
+        )
+        mean_kernel_generated = tf.reduce_sum(
+            kernel_generated * (1.0 - tf.eye(batch_size))
+        ) / (batch_size_f * (batch_size_f - 1.0))
+        mean_kernel_cross = tf.reduce_mean(kernel_cross)
+        kid = mean_kernel_real + mean_kernel_generated - 2.0 * mean_kernel_cross
+
+        # update the average KID estimate
+        self.kid_tracker.update_state(kid)
+
+    def result(self):
+        return self.kid_tracker.result()
+
+    def reset_state(self):
+        self.kid_tracker.reset_state()
+
+
+def get_network():
+    def EmbeddingLayer(min_frequency=1.0, max_frequency=1000.0):
+        def sinusoidal_embedding(x):
+            frequencies = tf.exp(
+                tf.linspace(
+                    tf.math.log(min_frequency),
+                    tf.math.log(max_frequency),
+                    embedding_dims // 2,
+                )
+            )
+            angular_speeds = 2.0 * math.pi * frequencies
+            embeddings = tf.concat(
+                [tf.sin(angular_speeds * x), tf.cos(angular_speeds * x)],
+                axis=3,
+            )
+            return embeddings
+
+        def forward(x):
+            x = layers.Lambda(sinusoidal_embedding)(x)
+            return x
+
+        return forward
+
+    def ResidualBlock(width):
+        def forward(x):
+            input_width = x.shape[3]
+            if input_width == width:
+                residual = x
+            else:
+                residual = layers.Conv2D(width, kernel_size=1)(x)
+            x = layers.BatchNormalization(center=False, scale=False)(x)
+            x = layers.Conv2D(
+                width, kernel_size=3, padding="same", activation=keras.activations.swish
+            )(x)
+            x = layers.Conv2D(width, kernel_size=3, padding="same")(x)
+            x = layers.Add()([residual, x])
+            return x
+
+        return forward
+
+    def DownBlock(block_depth, width):
+        def forward(x):
+            x, skips = x
+            for _ in range(block_depth):
+                x = ResidualBlock(width)(x)
+                skips.append(x)
+            x = layers.AveragePooling2D(pool_size=2)(x)
+            return x
+
+        return forward
+
+    def UpBlock(block_depth, width):
+        def forward(x):
+            x, skips = x
+            x = layers.UpSampling2D(size=2, interpolation="bilinear")(x)
+            for _ in range(block_depth):
+                x = layers.Concatenate()([x, skips.pop()])
+                x = ResidualBlock(width)(x)
+            return x
+
+        return forward
+
+    images = keras.Input(shape=(image_size, image_size, 3))
+    noise_rates = keras.Input(shape=(1, 1, 1))
+
+    x = layers.Conv2D(widths[0], kernel_size=1)(images)
+    skips = [x]
+
+    n = EmbeddingLayer()(noise_rates)
+    n = layers.UpSampling2D(size=image_size, interpolation="nearest")(n)
+    x = layers.Concatenate()([x, n])
+
+    for width in widths[:-1]:
+        x = DownBlock(block_depth, width)([x, skips])
+
+    for _ in range(block_depth):
+        x = ResidualBlock(widths[-1])(x)
+
+    for width in reversed(widths[:-1]):
+        x = UpBlock(block_depth, width)([x, skips])
+
+    x = layers.Concatenate()([x, skips.pop()])
+    x = layers.Conv2D(3, kernel_size=1, kernel_initializer="zeros")(x)
+
+    return keras.Model([images, noise_rates], x, name="residual_unet")
+
+
+class DiffusionModel(keras.Model):
+    def __init__(self):
+        super().__init__()
+
+        self.normalizer = layers.Normalization()
+        self.network = get_network()
+        self.ema_network = keras.models.clone_model(self.network)
+
+        self.initial_noise = None
+
+    def compile(self, **kwargs):
+        super().compile(**kwargs)
+
+        self.noise_loss_tracker = keras.metrics.Mean(name="n_loss")
+        self.image_loss_tracker = keras.metrics.Mean(name="i_loss")
+        self.kid = KID()
+
+    @property
+    def metrics(self):
+        return [self.noise_loss_tracker, self.image_loss_tracker, self.kid]
+
+    def denormalize(self, images):
+        images = self.normalizer.mean + (images * self.normalizer.variance ** 0.5)
+        return tf.clip_by_value(images, 0.0, 1.0)
+
+    def noise_schedule(self, diffusion_times):
+        max_angle = tf.acos(min_signal_rate)
+        diffusion_angles = diffusion_times * max_angle
+
+        signal_rates = tf.cos(diffusion_angles)
+        noise_rates = tf.sin(diffusion_angles)
+
+        return noise_rates, signal_rates
+
+    def denoise(self, noisy_images, noise_rates, signal_rates, training):
+        if training:
+            network = self.network
+        else:
+            network = self.ema_network
+
+        pred_noises = network([noisy_images, noise_rates], training=training)
+        pred_images = (noisy_images - noise_rates * pred_noises) / signal_rates
+
+        return pred_noises, pred_images
+
+    def reverse_diffusion(self, initial_noise, diffusion_steps):
+        num_images = initial_noise.shape[0]
+        step_size = 1.0 / diffusion_steps
+
+        noisy_images = initial_noise
+        for step in range(diffusion_steps):
+            diffusion_times = tf.ones((num_images, 1, 1, 1)) - step * step_size
+            noise_rates, signal_rates = self.noise_schedule(diffusion_times)
+            pred_noises, pred_images = self.denoise(
+                noisy_images, noise_rates, signal_rates, training=False
+            )
+
+            next_diffusion_times = diffusion_times - step_size
+            next_noise_rates, next_signal_rates = self.noise_schedule(
+                next_diffusion_times
+            )
+            noisy_images = (
+                next_signal_rates * pred_images + next_noise_rates * pred_noises
+            )
+
+        return pred_images
+
+    def generate(self, num_images, diffusion_steps, plot=False):
+        if plot:
+            if self.initial_noise is None:
+                self.initial_noise = tf.random.normal(
+                    shape=(num_images, image_size, image_size, 3)
+                )
+            initial_noise = self.initial_noise
+        else:
+            initial_noise = tf.random.normal(
+                shape=(num_images, image_size, image_size, 3)
+            )
+        generated_images = self.reverse_diffusion(initial_noise, diffusion_steps)
+        generated_images = self.denormalize(generated_images)
+        return generated_images
+
+    def train_step(self, images):
+        images = self.normalizer(images, training=True)
+        noises = tf.random.normal(shape=(batch_size, image_size, image_size, 3))
+
+        diffusion_times = tf.random.uniform(
+            shape=(batch_size, 1, 1, 1), minval=0.0, maxval=1.0
+        )
+        noise_rates, signal_rates = self.noise_schedule(diffusion_times)
+        noisy_images = signal_rates * images + noise_rates * noises
+
+        with tf.GradientTape() as tape:
+            pred_noises, pred_images = self.denoise(
+                noisy_images, noise_rates, signal_rates, training=True
+            )
+
+            noise_loss = self.loss(noises, pred_noises)
+            image_loss = self.loss(images, pred_images)
+
+        gradients = tape.gradient(noise_loss, self.network.trainable_weights)
+        self.optimizer.apply_gradients(zip(gradients, self.network.trainable_weights))
+
+        self.noise_loss_tracker.update_state(noise_loss)
+        self.image_loss_tracker.update_state(image_loss)
+
+        for weight, ema_weight in zip(self.network.weights, self.ema_network.weights):
+            ema_weight.assign(ema * ema_weight + (1 - ema) * weight)
+
+        # KID is not measured during the training phase for computational efficiency
+        return {m.name: m.result() for m in self.metrics[:-1]}
+
+    def test_step(self, images):
+        images = self.normalizer(images, training=False)
+        noises = tf.random.normal(shape=(batch_size, image_size, image_size, 3))
+
+        diffusion_times = tf.random.uniform(
+            shape=(batch_size, 1, 1, 1), minval=0.0, maxval=1.0
+        )
+        noise_rates, signal_rates = self.noise_schedule(diffusion_times)
+        noisy_images = signal_rates * images + noise_rates * noises
+
+        pred_noises, pred_images = self.denoise(
+            noisy_images, noise_rates, signal_rates, training=False
+        )
+
+        noise_loss = self.loss(noises, pred_noises)
+        image_loss = self.loss(images, pred_images)
+
+        self.image_loss_tracker.update_state(image_loss)
+        self.noise_loss_tracker.update_state(noise_loss)
+
+        images = self.denormalize(images)
+        generated_images = self.generate(
+            num_images=batch_size, diffusion_steps=kid_diffusion_steps
+        )
+        self.kid.update_state(images, generated_images)
+
+        return {m.name: m.result() for m in self.metrics}
+
+    def plot_images(self, epoch=-1, logs=None, num_rows=3, num_cols=6, num_plots=5):
+        generated_images = self.generate(
+            num_images=num_plots * num_rows * num_cols,
+            diffusion_steps=plot_diffusion_steps,
+            plot=True,
+        )
+
+        for plot in range(num_plots):
+            plt.figure(figsize=(num_cols * 2.0, num_rows * 2.0))
+            for row in range(num_rows):
+                for col in range(num_cols):
+                    index = row * num_cols + col
+                    plt.subplot(num_rows, num_cols, index + 1)
+                    plt.imshow(generated_images[plot * num_rows * num_cols + index])
+                    plt.axis("off")
+            plt.tight_layout()
+            plt.savefig("images/gif/{}_{}.png".format(plot, epoch + 1))
+            plt.close()
+
+
+# create and compile the model
+model = DiffusionModel()
+model.compile(
+    optimizer=tfa.optimizers.AdamW(
+        learning_rate=learning_rate, weight_decay=weight_decay
+    ),
+    loss=keras.losses.mean_absolute_error,
+)
+
+# save the best model based on the validation KID metric
+checkpoint_path = "checkpoints/diffusion_model"
+checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_path,
+    save_weights_only=True,
+    monitor="val_kid",
+    mode="min",
+    save_best_only=True,
+)
+
+# run training and plot generated images periodically
+model.normalizer.adapt(train_dataset)
+model.plot_images()
+model.fit(
+    train_dataset,
+    epochs=num_epochs,
+    validation_data=val_dataset,
+    callbacks=[
+        keras.callbacks.LambdaCallback(on_epoch_end=model.plot_images),
+        checkpoint_callback,
+    ],
+)
+
+# load best model and generate images
+# model.load_weights(checkpoint_path)
+# model.plot_images()
+
+# model.evaluate(val_dataset)
